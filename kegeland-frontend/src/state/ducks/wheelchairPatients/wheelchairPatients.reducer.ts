d14cd8404755b72eb1d11d0e02eb794673ee9479
William <wamarlea@ntnu.no>
2023-09-28 14:13:37 +0200

    Added wheelcharPatients reducer
    Added a simple  wheelchairPatients reducer

--- kegeland-frontend/src/state/ducks/wheelchairPatients/wheelchairPatients.reducer.ts
+++ kegeland-frontend/src/state/ducks/wheelchairPatients/wheelchairPatients.reducer.ts
@@ -1,1 +1,62 @@
-// TODO: find out what reducer file does
+/*
+
+In this reducer:
+
+We defined an interface WheelchairPatientsState which includes:
+loading: a boolean indicating whether data is being loaded.
+wheelchairPatients: representing the list of all wheelchair patients.
+wheelchairPatient: representing a single wheelchair patient.
+error: a string to store any error messages.
+The initialState is defined based on WheelchairPatientsState.
+We created a slice using createSlice.
+Inside extraReducers, we handle different states of the fetchWheelchairPatientById async action:
+When the action is pending, loading is set to true and error is reset.
+When the action is fulfilled, loading is set to false, wheelchairPatient is updated with the payload, and error is reset.
+When the action is rejected, loading is set to false and error is updated with the error message.
+
+This structure allows you to manage the state for individual wheelchair patients and handle the loading state and possible errors during fetch operations. You can similarly handle actions for fetching multiple wheelchair patients if such actions are defined.
+
+*/
+
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { WheelchairPatients, WheelchairPatient } from './wheelchairPatients.interface';
+import { fetchWheelchairPatientById } from './wheelchairPatients.actions';
+
+interface WheelchairPatientsState {
+  loading: boolean;
+  wheelchairPatients: WheelchairPatients | null;
+  wheelchairPatient: WheelchairPatient | null;
+  error: string | null;
+}
+
+const initialState: WheelchairPatientsState = {
+  loading: false,
+  wheelchairPatients: null,
+  wheelchairPatient: null,
+  error: null,
+};
+
+const wheelchairPatientsSlice = createSlice({
+  name: 'wheelchairPatients',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWheelchairPatientById.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchWheelchairPatientById.fulfilled, (state, action: PayloadAction<WheelchairPatient>) => {
+        state.loading = false;
+        state.wheelchairPatient = action.payload;
+        state.error = null;
+      })
+      .addCase(fetchWheelchairPatientById.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || 'An error occurred';
+      });
+  },
+});
+
+export default wheelchairPatientsSlice.reducer;

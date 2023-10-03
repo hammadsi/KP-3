949b52f5de2ad8100e2edeb170be93fe4738fcbb
William <wamarlea@ntnu.no>
2023-10-03 15:57:57 +0200

    Made component for printing wheelchairPatientID

---
+++ kegeland-frontend/src/components/WheelchairPatient.tsx
@@ -0,0 +1,25 @@
+// WheelchairPatientComponent.tsx
+import React from 'react';
+import useWheelchairPatientById from '../hooks/useWheelchairPatient';
+
+interface WheelchairPatientComponentProps {
+  id: string;
+}
+
+const WheelchairPatientComponent: React.FC<WheelchairPatientComponentProps> = ({ id }) => {
+  const { wheelchairPatient, loading, error } = useWheelchairPatientById(id);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+  if (!wheelchairPatient) return <p>No patient found</p>;
+
+  return (
+    <div>
+      <h2>Wheelchair Patient</h2>
+      <p>ID: {wheelchairPatient.patientId}</p>
+      {/* Other patient data... */}
+    </div>
+  );
+};
+
+export default WheelchairPatientComponent;

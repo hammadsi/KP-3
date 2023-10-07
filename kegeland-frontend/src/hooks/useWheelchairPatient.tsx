09a8b1e3ad54717d5abe5cf1592ca27c1df80943
William <wamarlea@ntnu.no>
2023-10-07 22:50:35 +0200

    Updated hook to avoid infinite loop

--- kegeland-frontend/src/hooks/useWheelchairPatient.tsx
+++ kegeland-frontend/src/hooks/useWheelchairPatient.tsx
@@ -6,32 +6,46 @@ import useAppSelector from './useAppSelector';
 
 const useWheelchairPatient = (patientId: string) => {
   const dispatch = useAppDispatch();
-  const [loading, setLoading] = useState<boolean>(true); 
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
   const { wheelchairPatients } = useAppSelector((state) => state);
-  
-  useEffect(() => {
-    dispatch(fetchWheelchairPatientById(patientId));
 
-    // Cleanup function to reset state on component unmount
+  useEffect(() => {
+    // Reset error state upon new request
+    setError(null);
+
+    // Only fetch new patient data if the ID changes, or if the data isn't present in the state
+    if (!wheelchairPatients.wheelchairPatient || wheelchairPatients.wheelchairPatient.id !== patientId) {
+      dispatch(fetchWheelchairPatientById(patientId))
+        .catch((e: Error) => {
+          setError(`Failed to fetch patient: ${e.message}`);
+        });
+    }
+  }, [dispatch, patientId, wheelchairPatients.wheelchairPatient]);
+
+  //NOTE: without a dedicated useEffect for clean-up, an infinite loop of API-calls will be triggered
+  useEffect(() => {
+    // Cleanup on unmount only
     return () => {
       dispatch(clearWheelchairPatientsState());
     };
-  }, [dispatch, patientId]);
+  }, [dispatch]);
 
   useEffect(() => {
     // Ensure local loading state is only false when the data corresponds to the requested ID
-    if (loading && !wheelchairPatients.loading && wheelchairPatients.wheelchairPatient?.patientId === patientId) {
+    if (loading && !wheelchairPatients.loading && wheelchairPatients.wheelchairPatient?.id === patientId) {
       setLoading(false);
-    } else if (!loading && (wheelchairPatients.loading || wheelchairPatients.wheelchairPatient?.patientId !== patientId)) {
+    } else if (!loading && (wheelchairPatients.loading || wheelchairPatients.wheelchairPatient?.id !== patientId)) {
       setLoading(true);
     }
-  }, [wheelchairPatients.loading, wheelchairPatients.wheelchairPatient]);
+  }, [loading, wheelchairPatients.loading, wheelchairPatients.wheelchairPatient]);
 
   return {
     wheelchairPatient: wheelchairPatients.wheelchairPatient,
-    error: wheelchairPatients.error,
+    error: error || wheelchairPatients.error,
     loading,
   };
 };
+
 
 export default useWheelchairPatient;

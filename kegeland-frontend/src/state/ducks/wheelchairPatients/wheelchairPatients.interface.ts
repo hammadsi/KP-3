88bb3fd6db5cbfa7ff18287c5c21ffb0a6910b94
William <wamarlea@ntnu.no>
2023-10-07 22:50:14 +0200

    Made entity (API) and adapted interface (Frontend)

--- kegeland-frontend/src/state/ducks/wheelchairPatients/wheelchairPatients.interface.ts
+++ kegeland-frontend/src/state/ducks/wheelchairPatients/wheelchairPatients.interface.ts
@@ -1,58 +1,66 @@
-export type WheelchairPatients = {
-  wheelchairPatients: WheelchairPatient[];
-};
-
 export type WheelchairPatient = {
-  patientId: String;
-  name: String;
-  age: Number;
+  id: string;
+  name: string;
+  age: number;
   gender: 'M' | 'F' | 'O';
   currentPhysicalState: {
-    height: Number;
-    weight: Number;
-    maxHeartRate: Number;
-    averageHeartRate: Number;
-    maxWheelchairSpeed: Number;
-    averageWheelchairSpeed: Number;
+    height: number;
+    weight: number;
+    maxHeartRate: number;
+    averageHeartRate: number;
+    maxWheelchairSpeed: number;
+    averageWheelchairSpeed: number;
   };
-  gameSessions: {
-    sessionId: String;
-    //exerciseTime should be calcuated from startTime and endTime
-    exerciseTime: Number;
-    startTime: Date;
-    endTime: Date;
-    questionaires: {
-      preGame: {
-        question: String;
-        answer: String;
-      }[];
-      postGame: {
-        question: String;
-        answer: String;
-      }[];
-    };
-    laps: {
-      lapTime: Number;
-      timeStamp: Date;
-    }[];
-    timeSeriesData: {
-      heartRates: {
-        heartRate: Number;
-        timestamp: Date;
-      }[];
-      speeds: {
-        leftSpeed: Number;
-        rightSpeed: Number;
-        timestamp: Date;
-      }[];
-      imus: {}[];
-    };
-  }[];
+  gameSessions: GameSession[];
+};
+
+export type GameSession = {
+  sessionId: string;
+  exerciseTime: number;  // Calculated from startTime and endTime
+  startTime: Date;
+  endTime: Date;
+  questionaires: {
+    preGame: Questionaire[];
+    postGame: Questionaire[];
+  };
+  laps: Lap[];
+  timeSeriesData: TimeSeriesData;
+};
+
+export type Questionaire = {
+  question: string;
+  answer: string;
+};
+
+export type Lap = {
+  lapTime: number;
+  timeStamp: Date;
+};
+
+export type TimeSeriesData = {
+  heartRates: HeartRate[];
+  speeds: Speed[];
+  imus: IMUData[];  // Define the IMUData type based on actual data structure
+};
+
+export type HeartRate = {
+  heartRate: number;
+  timestamp: Date;
+};
+
+export type Speed = {
+  leftSpeed: number;
+  rightSpeed: number;
+  timestamp: Date;
+};
+
+export type IMUData = {
+  // Define properties based on your requirements and data structure
 };
 
 export type WheelchairPatientsState = {
   loading: boolean;
-  wheelchairPatients: WheelchairPatients | null;
-  wheelchairPatient: WheelchairPatient | null;
-  error: string | null;
+  wheelchairPatient: WheelchairPatient | undefined;
+  data: WheelchairPatient[];
+  error: string | undefined;
 };

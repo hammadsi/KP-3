87ec8d3ea9a7a98e47620bdcdcdfc4ba81e88b3c
simeonc01 <simeonc@stud.ntnu.no>
2023-10-11 23:05:10 +0200

    Added input fields on edit page

--- kegeland-frontend/src/pages/profile/EditProfilePage.tsx
+++ kegeland-frontend/src/pages/profile/EditProfilePage.tsx
@@ -1,169 +1,165 @@
 import { useNavigate, useParams } from 'react-router-dom';
+import { Button, Stack, FormControl, FormLabel, Select } from '@chakra-ui/react';
 
 import Card from '../../components/Card';
 import withLayout from '../../hoc/withLayout';
 import withSpinner from '../../hoc/withSpinner';
-import useExercise from '../../hooks/useExercise';
-import { Box, Container, HStack, Heading, Link, VStack } from '@chakra-ui/react';
-import { Formik } from 'formik';
-import { InputControl, SubmitButton } from 'formik-chakra-ui';
-import useAppSelector from '../../hooks/useAppSelector';
-import useAppDispatch from '../../hooks/useAppDispatch';
+import useWheelchairPatient from '../../hooks/useWheelchairPatient';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../state/store';
+import { Input } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { type } from 'os';
 
-type FormDataUserDetails = {
+
+
+
+type Gender = 'M' | 'F' | 'O';
+
+type FormData = {
   firstName: string;
   lastName: string;
+  gender: Gender;
+  birthofdate: Date;
+  height: number;
+  weight: number;
 };
 
-type FormDataPatient = {
-  firstName: string;
-  lastName: string;
-  email: string;
-};
+
 
 
 const EditProfilePage = () => {
 
-    const { error, loading } = useAppSelector((state) => state.auth);
-    const dispatch = useAppDispatch();
-    const { isSignedIn } = useAppSelector((state) => state.auth);
+    const { authUser } = useSelector((state: RootState) => state.auth);
+
+    const { wheelchairPatient, error, loading } = useWheelchairPatient(authUser?.id);
+
     const navigate = useNavigate();
+
+    const update = (data: FormData) => {
+      // TODO: Implement update logic
+      // Send data to API
+      // navigate to '/myprofile';
+    };
+
+    useEffect(() => {
+      if (wheelchairPatient) {
+        const { name, age, gender, currentPhysicalState } = wheelchairPatient;
+          setFormData({
+              firstName: name || '',
+              lastName: name || '',
+              gender: gender || 'M',
+              birthofdate: new Date(),
+              height: currentPhysicalState.height || 0,
+              weight: currentPhysicalState.weight || 0,
+          });
+      }
+  }, [wheelchairPatient]);
+
+    
+  
+    const [formData, setFormData] = useState<FormData>({
+      firstName: '',
+      lastName: '',
+      gender: 'M' as Gender,
+      birthofdate: new Date(),
+      height: 0,
+      weight: 0,
+    });
+  
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+      if (e.target.name === "birthofdate") {
+        const newDate = new Date(e.target.value);
+        setFormData({
+          ...formData,
+          birthofdate: newDate
+        });
+      } else {
+        setFormData({
+          ...formData,
+          [e.target.name]: e.target.value,
+        });
+      }
+    };
+
+    // Denne må oppdateres med alle feltene som skal sjekkes (Date og Select)
+    const allFilled = (): boolean => {
+      return Object.values(formData).every(field => {
+        if (typeof field === 'string') {
+          return field.trim() !== ''; // Check if string fields are not empty
+        }
+        if (typeof field === 'number') {
+          return field !== 0; // Assume 0 is not a valid value. Adjust if not the case.
+        }
+        return true; // Assumes other fields (like Date) are always valid
+      });
+    };
+
+    const formatDate = (date: Date) => {
+      const yyyy = date.getFullYear().toString();
+      const mm = (date.getMonth() + 1).toString().padStart(2, '0');
+      const dd = date.getDate().toString().padStart(2, '0');
+      return `${yyyy}-${mm}-${dd}`;
+    };
     
   
     return (
       <>
-      <Card loading={loading} minH="lg">
-        <div 
-          style={{
-            display: 'flex', 
-            justifyContent: 'space-between', 
-            marginLeft: '20px', 
-            marginRight: '20px', 
-            marginBottom: '20px', 
-            padding: '10px', 
-            borderBottom: '1.5px solid gray'}}>
-          <h1 style={{fontWeight: 'bold'}}>About Me</h1>  
-        </div>
-        <Container>
-          <Container paddingTop="1em">
-            <Formik
-              onSubmit={async (values) => {
-                update(values);
-              }}
-              initialValues={{
-                firstName: '',
-                lastName: '',
-                gender: '',
-                birthofdate: '',
-                height: '',
-                weight: '',
-              }}
-              validationSchema={validationSchema}>
-              {(formProps) => (
-                <Box
-                  borderWidth="1px"
-                  rounded="lg"
-                  shadow="1px 1px 3px rgba(0,0,0,0.3)"
-                  maxWidth={800}
-                  p={6}
-                  m="10px auto"
-                  as="form"
-                  onSubmit={(e: any) => {
-                    e.preventDefault();
-                    formProps.handleSubmit();
-                  }}>
-                  <VStack spacing={5} align="stretch">
-                    <Box>
-                      <Heading as="h3" size="lg" textAlign="center">
-                        Register
-                      </Heading>
-                    </Box>
-                    <Box>
-                      <Heading as="h3" size="lg" textAlign="center">
-                        Please enter email and password to create a user.
-                      </Heading>
-                    </Box>
-                    <Box>
-                      <InputControl
-                        inputProps={{
-                          type: 'email',
-                          placeholder: 'ola.nordmann@example.com',
-                        }}
-                        name="email"
-                        isRequired
-                        label="Email address"
-                        data-testid="email-input"
-                      />
-                    </Box>
-                    <HStack justifyContent="space-between" w="100%">
-                      <Box>
-                        <InputControl
-                          inputProps={{
-                            type: 'text',
-                            placeholder: 'Ola',
-                          }}
-                          name="firstName"
-                          isRequired
-                          label="First name"
-                          data-testid="firstname-input"
-                        />
-                      </Box>
-                      <Box>
-                        <InputControl
-                          inputProps={{
-                            type: 'text',
-                            placeholder: 'Nordmann',
-                          }}
-                          isRequired
-                          label="Last name"
-                          data-testid="lastname-input"
-                          name="lastName"
-                        />
-                      </Box>
-                    </HStack>
-                    <Box>
-                      <InputControl
-                        isRequired
-                        name="password"
-                        label="Password"
-                        data-testid="password-input"
-                        inputProps={{
-                          type: 'password',
-                          autoComplete: 'new-password',
-                          placeholder: '• • • • • • • •',
-                        }}
-                        helperText="Create a new password."
-                      />
-                    </Box>
-                    <Box>
-                      <InputControl
-                        isRequired
-                        name="confirmPassword"
-                        data-testid="confirmPassword-input"
-                        inputProps={{
-                          type: 'password',
-                          autoComplete: 'new-password',
-                          placeholder: '• • • • • • • •',
-                        }}
-                        helperText="Repeat your password."
-                      />
-                    </Box>
-                    <Box color={'red'}>{error}</Box>
-                    <Box textAlign="right">
-                      <SubmitButton
-                        colorScheme="primary"
-                        isLoading={formProps.isSubmitting || loading}
-                        isDisabled={!formProps.isValid}>
-                        Register user
-                      </SubmitButton>
-                    </Box>
-                  </VStack>
-                </Box>
-              )}
-            </Formik>
-          </Container>
-        </Container>
-      </Card>
+        <Card loading={loading} minH="lg">
+          <div 
+            style={{
+              display: 'flex', 
+              justifyContent: 'space-between', 
+              marginLeft: '20px', 
+              marginRight: '20px', 
+              marginBottom: '20px', 
+              padding: '10px', 
+              borderBottom: '1.5px solid gray'}}>
+            <h1 style={{fontWeight: 'bold'}}>About Me</h1>  
+          </div>
+          <div style={{marginLeft: '20%', marginRight: '20%'}}>
+            <Stack spacing={10} direction="row" mt={6}>
+              <FormControl>
+                <FormLabel>First Name</FormLabel>
+                <Input name="firstName" value={formData.firstName} onChange={handleChange} />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Last Name</FormLabel>
+                <Input name="lastName" value={formData.lastName} onChange={handleChange} />
+              </FormControl>
+            </Stack>
+            <Stack spacing={10} direction="row" mt={6}>
+              <FormControl id="gender">
+                <FormLabel>Gender</FormLabel>
+                <Select
+                  name="gender"
+                  onChange={handleChange}>
+                  <option value="M">Male</option>
+                  <option value="F">Female</option>
+                  <option value="O">Other</option>
+                </Select>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Birth of Date</FormLabel>
+                <Input name="birthofdate" type="date" value={formatDate(formData.birthofdate)} onChange={handleChange} />
+              </FormControl>
+            </Stack>
+            <Stack spacing={10} direction="row" mt={6}>
+              <FormControl>
+                <FormLabel>Height</FormLabel>
+                <Input name="height" type='number' value={formData.height} onChange={handleChange} />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Weight</FormLabel>
+                <Input name="weight" value={formData.weight} onChange={handleChange} />
+              </FormControl>
+            </Stack>
+            <Stack direction="row" spacing={10} mt={10} justifyContent="center">
+              <Button colorScheme="gray" onClick={() => navigate('/myprofile')}>Cancel</Button>
+              <Button colorScheme="blue" disabled={!allFilled()} onClick={() => update(formData)}>Submit</Button>
+            </Stack>
+          </div>
+        </Card>
       </>
     );
   };

62d3ccd036f7fe1410a17585044b937a670b1887
simeonc01 <simeonc@stud.ntnu.no>
2023-11-15 11:16:07 +0100

    Bug fixed

--- kegeland-frontend/src/components/Graph/GraphHeader.tsx
+++ kegeland-frontend/src/components/Graph/GraphHeader.tsx
@@ -25,7 +25,6 @@ const GraphHeader: React.FC<GraphHeaderProps> = ({ date, endTime }) => {
       (durationInMilliseconds % (1000 * 60 * 60)) / (1000 * 60),
     );
     const seconds = Math.floor((durationInMilliseconds % (1000 * 60)) / 1000);
-
     return `${hours}:${String(minutes).padStart(2, '0')}:${String(
       seconds,
     ).padStart(2, '0')}`;
